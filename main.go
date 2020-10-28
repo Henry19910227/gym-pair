7a5488492e1e114bb0c4034fd867e8ccce8f2bcc
Henry.Liao <Henry.Liao@skylinetw.com>
2020-10-28 20:26:10 +0800

    加入 gin-swagger

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ import (
 	"github.com/Henry19910227/gym-pair/pkg/logger"
 	"github.com/Henry19910227/gym-pair/pkg/upload"
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/Henry19910227/gym-pair/docs"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 var (
@@ -32,11 +36,27 @@ func init() {
 	setupTokenTool()
 }
 
+// @title Henry
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
 func main() {
 	router := gin.New()
-	router.Use(gin.CustomRecovery(middleware.Recover())) //加入攔截panic中間層
-	router.Use(gin.Logger())                             //加入路由Logger
-	router.Use(middleware.Cors())                        //加入解決跨域中間層
+	router.Use(gin.CustomRecovery(middleware.Recover()))            //加入攔截panic中間層
+	router.Use(gin.Logger())                                        //加入路由Logger
+	router.Use(middleware.Cors())                                   //加入解決跨域中間層
+	url := ginSwagger.URL("http://localhost:9090/swagger/doc.json") // The url pointing to API definition
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 	controller.NewUserController(router, userService, jwtTool)
 	controller.NewLoginController(router, loginService, jwtTool)
 
